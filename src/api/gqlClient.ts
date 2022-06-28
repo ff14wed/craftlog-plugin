124f3757be5dd6021c29875ddd73e176224636d5
Flawed <33593723+ff14wed@users.noreply.github.com>
2022-06-28 10:42:24 -0700

    Update apollo client

--- src/api/gqlClient.ts
+++ src/api/gqlClient.ts
@@ -1,9 +1,10 @@
 import gql from './gql';
 
-import { ApolloLink, execute, makePromise, GraphQLRequest } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { onError } from "apollo-link-error";
+import { ApolloLink, ApolloClient, InMemoryCache, HttpLink, from, split, type NormalizedCacheObject } from "@apollo/client/core/index.js";
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { onError } from "@apollo/client/link/error";
 
 const errHandlerLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
@@ -15,24 +16,22 @@ const errHandlerLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-const executeOperation = (httpLink: ApolloLink, operation: GraphQLRequest) => {
-  // Funnel errors into a single promise rejection
-  return new Promise<{ [key: string]: any }>((resolve, reject) => {
-    makePromise(execute(httpLink, operation))
-      .then((result) => {
-        if (result.data) {
-          resolve(result.data);
-        } else {
-          reject(result.errors);
+const authLink = (apiToken?: string): ApolloLink => {
+  return new ApolloLink((operation, forward) => {
+    if (apiToken) {
+      operation.setContext(({ headers }: { headers: any }) => ({
+        headers: {
+          Authorization: apiToken,
+          ...headers
         }
-      })
-      .catch((error) => reject(error));
-  })
+      }));
+    }
+    return forward(operation);
+  });
 };
 
 export default class GQLClient {
-  private httpLink: ApolloLink;
-  private wsLink: ApolloLink;
+  private client: ApolloClient<NormalizedCacheObject>;
 
   constructor(apiURL: string, apiToken?: string) {
     let httpOpts = (apiToken) ? {
@@ -44,71 +43,104 @@ export default class GQLClient {
       authorization: apiToken,
     }) : undefined;
 
-    this.httpLink = ApolloLink.from([
-      errHandlerLink,
-      new HttpLink({ uri: apiURL, ...httpOpts }),
-    ]);
-    this.wsLink = ApolloLink.from([
-      errHandlerLink,
-      new WebSocketLink({
-        uri: apiURL.replace('http://', 'ws://'),
-        options: {
-          reconnect: true,
-          connectionParams: wsConnectionParams,
+    const httpLink = from([errHandlerLink, authLink(apiToken), new HttpLink({
+      uri: apiURL,
+    })]);
+
+    const wsLink = from([errHandlerLink, new GraphQLWsLink(createClient({
+      url: apiURL.replace('http://', 'ws://'),
+      retryAttempts: 10,
+      connectionParams: wsConnectionParams
+    }
+    ))]);
+
+    const splitLink = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        );
+      },
+      wsLink,
+      httpLink,
+    );
+
+    this.client = new ApolloClient({
+      link: splitLink,
+      cache: new InMemoryCache(),
+      defaultOptions: {
+        query: {
+          fetchPolicy: 'no-cache',
         },
-      }),
-    ]);
+      }
+    });
   }
 
   public getAPIVersion = async (): Promise<string> => {
-    const data = await executeOperation(
-      this.httpLink, { query: gql.versionQuery },
-    );
-    return data.apiVersion;
+    const result = await this.client.query({ query: gql.versionQuery });
+    if (result.error || result.errors) {
+      console.error("Encountered error getting API Version");
+    }
+    return result.data.apiVersion;
   }
 
   public listStreams = async () => {
-    const data = await executeOperation(
-      this.httpLink, { query: gql.listStreamsQuery },
-    )
-    return data.streams;
-  }
+    const result = await this.client.query({ query: gql.listStreamsQuery });
+    if (result.error || result.errors) {
+      console.error("Encountered error getting streams");
+    }
+    return result.data.streams;
+  };
 
   public getStream = async (streamID: number) => {
-    const variables = { streamID }
-    const data = await executeOperation(
-      this.httpLink, { query: gql.streamQuery, variables },
-    )
-    return data.stream;
+    const variables = { streamID };
+
+    const result = await this.client.query({ query: gql.streamQuery, variables });
+    if (result.error || result.errors) {
+      console.error("Encountered error getting stream");
+    }
+
+    return result.data.stream;
   }
 
   public subscribeToStreamEvents = (
     streamID: number, handleStreamEvent: (type: string, data: any) => void
-  ) =>
-    execute(this.wsLink, { query: gql.streamSubscription })
-      .subscribe({
-        next: (subscriptionData) => {
-          if (!subscriptionData.data) { return; }
-          let streamEvent = subscriptionData.data.streamEvent;
-          if (streamEvent.streamID !== streamID) { return; }
-          let { __typename, ...eventData } = streamEvent.type;
-          handleStreamEvent(__typename, eventData);
-        },
-      });
+  ) => {
+    this.client.subscribe({ query: gql.streamSubscription }).subscribe({
+      next: this.createStreamEventCB(streamID, handleStreamEvent),
+      error: (err) => console.log("Stream subscription err:", err),
+    });
+  }
+
+  createStreamEventCB = (streamID: number, cb: (type: string, data: any) => void) => {
+    return (subscriptionData: any) => {
+      if (!subscriptionData.data) { return; }
+      let streamEvent = subscriptionData.data.streamEvent;
+      if (streamEvent.streamID !== streamID) { return; }
+      let { __typename, ...eventData } = streamEvent.type;
+      cb(__typename, eventData);
+    }
+  }
 
   public subscribeToEntityEvents = (
     streamID: number, handleEntityEvent: (entityID: number, type: string, data: any) => void,
-  ) =>
-    execute(this.wsLink, { query: gql.entitySubscription })
-      .subscribe({
-        next: (subscriptionData) => {
-          if (!subscriptionData.data) { return; }
-          let entityEvent = subscriptionData.data.entityEvent;
-          if (entityEvent.streamID !== streamID) { return; }
+  ) => {
+    this.client.subscribe({ query: gql.entitySubscription }).subscribe({
+      next: this.createEntityEventCB(streamID, handleEntityEvent),
+      error: (err) => console.log("Entity subscription err:", err),
+    });
+  }
 
-          let { entityID } = entityEvent;
-          let { __typename, ...eventData } = entityEvent.type;
-          handleEntityEvent(entityID, __typename, eventData);
-        },
-      });
+  createEntityEventCB = (streamID: number, cb: (entityID: number, type: string, data: any) => void) => {
+    return (subscriptionData: any) => {
+      if (!subscriptionData.data) { return; }
+      let entityEvent = subscriptionData.data.entityEvent;
+      if (entityEvent.streamID !== streamID) { return; }
+
+      let { entityID } = entityEvent;
+      let { __typename, ...eventData } = entityEvent.type;
+      cb(entityID, __typename, eventData);
+    }
+  }
 }
